7fa03f4595d8cc222169e778c88a1d1af93e959f
mandarinsoda <matthewjcarlson@gmail.com>
2012-06-29 15:05:08 -0700

    photos

--- web.js
+++ web.js
@@ -2,8 +2,28 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 var port = process.env.PORT || 5000;
