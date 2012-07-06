d4d87504e5c33851a703d61ef22986ae1a22712b
mandarinsoda <matthewjcarlson@gmail.com>
2012-07-06 10:04:11 -0700

    images

--- web.js
+++ web.js
@@ -37,7 +37,7 @@ app.post('/save', function (req, res) {
 
 app.get('/aviary', function (req, res) {
    var ts = Math.round((new Date()).getTime() / 1000);
-   var BLAH = "{'metadata':{'imageorigsize':[1000,667]},'actionlist':[{'action':'setfeathereditsize','width':1000,'height':667},{'action':'joecool','params':[],'flatten':true}]}";
+   var BLAH = "{'metadata':{'imageorigsize':[1000,654]},'actionlist':[{'action':'setfeathereditsize','width':1000,'height':654},{'action':'andy','params':[],'flatten':true}]}";
 
    var params = 
         { api_key: 'd3954246e',
@@ -47,7 +47,7 @@ app.get('/aviary', function (req, res) {
           cellheight: -1,
           cellwidth: -1,
           cols: -1,
-          filepath: 'http://freezing-lightning-2366.herokuapp.com/jessi_weithman_family_raiter_014.jpg',
+          filepath: 'http://freezing-lightning-2366.herokuapp.com/jessi_weithman_family_raiter_018.jpg',
           filterid: -1,
           format: 'jpg',
           hardware_version: 1,
@@ -71,7 +71,7 @@ app.get('/aviary', function (req, res) {
                    "cellheight-1"+
                    "cellwidth-1"+
                    "cols-1"+
-                   "filepath"+ encodeURIComponent("http://freezing-lightning-2366.herokuapp.com/jessi_weithman_family_raiter_014.jpg")+
+                   "filepath"+ encodeURIComponent("http://freezing-lightning-2366.herokuapp.com/jessi_weithman_family_raiter_018.jpg")+
                    "filterid-1"+
                    "formatjpg"+
                    "hardware_version1"+
